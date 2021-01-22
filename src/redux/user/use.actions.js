4e6578c4ee5e7b2932a2481a17e3a12ab3a2fd50
Hassanferdous <hassanferdous101@gmial.com>
2021-01-22 23:49:30 +0600

    adsdf

--- src/redux/user/use.actions.js
+++ src/redux/user/use.actions.js
@@ -1,6 +1,6 @@
 import { userActionsTypes } from './user.types';
 
 export const setCurrrentUser = user => ({
-	type: userActionsTypes,
+	type: userActionsTypes.SET_CURRENT_USER,
 	payload: user,
 });
