2e9a59eded6a42e62af59b40fda1469b600af2e9
Hassanferdous <hassanferdous101@gmial.com>
2021-02-04 00:32:33 +0600

    adding storage in cart section

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,19 @@ import { combineReducers } from 'redux';
 import cartReducer from './cart/cartReducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+//Redux persit
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whiteList: ['cart'],
+};
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+// export out  updated rootReducer with reduxpersist
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
 import rootReducer from './rootReducer';
+
+//Redux Persist
+import { persistStore } from 'redux-persist';
 
 const middleWares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
